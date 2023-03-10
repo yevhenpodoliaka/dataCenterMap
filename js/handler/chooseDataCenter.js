cd2ca0072b7e66a4adfe171e5169acf1d18e4fc6
Yevhen Podoliaka <zanp85740@gmail.com>
2023-03-10 21:34:45 +0200

    added onStartBtn handler

--- js/handler/chooseDataCenter.js
+++ js/handler/chooseDataCenter.js
@@ -1,4 +1,9 @@
+import refs from "../refs.js"
 import dataCenterMarkup from "../markup/dataCenterMarkup.js";
+import onStartBtnClick from "./onStartBtnClick.js";
+
+let newCenter = 0;
+
 export default function chooseDataCenter(e) {
   const current = document.querySelector('[data-status="current"]');
 
@@ -6,12 +11,20 @@ export default function chooseDataCenter(e) {
     if (current !== null) {
       e.target.setAttribute("data-status", "new");
       e.target.innerHTML = dataCenterMarkup("new");
-        e.target.style.width = "50px";
-
+      e.target.style.width = "50px";
+      newCenter+=1;
+     if (newCenter === 2) {
+       refs.nextBtn.disabled = false;
+       refs.nextBtn.addEventListener("click",onStartBtnClick)
+     }
+  
     } else {
       e.target.setAttribute("data-status", "current");
       e.target.innerHTML = dataCenterMarkup("current");
-      e.target.style.width="50px"
+      e.target.style.width = "50px"
+      refs.message.textContent = "Choose minimum two additional spots for Byte Cloud and press"
+      refs.nextBtn.classList.remove("visually-hidden");
+      refs.nextBtn.disabled= true
     }
   }
 }
---
+++ js/handler/onStartBtnClick.js
@@ -0,0 +1,3 @@
+export default function onStartBtnClick() {
+    console.log("start")
+}
